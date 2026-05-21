b36f6e32219ea2a2de1629793f7854036d7818d2
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-05-21 14:08:39 +0530

    Add files via upload

---
+++ Array/ArrCmd.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class ArrCmd {
+    static void main(String[] args) {
+        int sum= 0 ;
+        if(args.length<2)
+            System.out.println("Please Enter 2 integer :");
+        else{//loops
+            for (int i = 0; i< args.length; i++){
+                int n = Integer.parseInt(args[i]);
+                sum = sum+n;
+            }
+            System.out.println(sum);
+        }
+    }
+}
