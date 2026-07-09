4b413195537a9883a5049d2e90b55662b13255d9
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-07-09 19:35:37 +0530

    Volume

--- OOPs/src/Overriding/UseCylinder.java
+++ OOPs/src/Overriding/UseCylinder.java
@@ -15,11 +15,14 @@ class Circle{
 class Cylinder extends Circle{
     private double heigth;
     public Cylinder(int radius , int height){
-        super(radius);
         this.heigth=height;
+        super(radius);
     }
     public double area(){
         return 2*super.area()+2*Math.PI*getRadius()*heigth;
+    }
+    public double volume(){
+        return super.area()*heigth;
     }
 }
 public class UseCylinder {
@@ -27,5 +30,6 @@ public class UseCylinder {
         Cylinder obj = new Cylinder(10,20);
         obj.area();
         System.out.println("Area of cylinder : "+obj.area());
+        System.out.println("Volume of cylinder : "+obj.volume());
     }
 }
