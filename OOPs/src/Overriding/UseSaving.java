9b8362420de7d3018f2330058c27bb471cb33f08
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-07-12 21:13:47 +0530

    Account

--- OOPs/src/Overriding/UseSaving.java
+++ OOPs/src/Overriding/UseSaving.java
@@ -9,6 +9,34 @@ class Account {
         this.name = name;
         this.balance = balance;
     }
+    public void displayInfo(){
+        System.out.print("AccId = "+accId+", Name = "+name+", Balance = "+balance);
+    }
+    public double getBalance(){
+        return balance;
+    }
+}
+class Saving extends Account{
+    private double intRate ;
+    public Saving(int accId , String name , double balance , double intRate ){
+        super(accId , name , balance);
+        this.intRate=intRate;
+    }
+    public double getIncome(){
+        double total = getBalance()+(getBalance()*intRate/100.0);
+        return total;
+    }
+    public void displayInfo(){
+       super.displayInfo();
+        System.out.println(", IntRate = "+intRate);
+    }
 }
 public class UseSaving {
+    static void main() {
+        Saving obj = new Saving(501, "HEX",570000.00, 20);
+        System.out.println("Original Balance : "+obj.getBalance());
+//        obj.getIncome();
+        obj.displayInfo();
+        System.out.println("Final Income : "+obj.getIncome());
+    }
 }
