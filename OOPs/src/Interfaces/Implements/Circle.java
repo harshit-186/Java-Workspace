75dd524b90e752d363a553138af85c558937be19
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-07-25 13:18:44 +0530

    Circle implements Shape

---
+++ OOPs/src/Interfaces/Implements/Circle.java
@@ -0,0 +1,4 @@
+package Interfaces.Implements;
+
+public class Circle {
+}
---
+++ OOPs/src/Interfaces/Implements/Shape.java
@@ -0,0 +1,12 @@
+package Interfaces.Implements;
+
+interface Shape {
+    double area ();
+    default double volume(){
+        System.out.println("don't have a volume of this figure");
+        return 0.0;
+    }
+    static void welComeMsg(){
+        System.out.println("Welcome Guyzz..!");
+    }
+}
