8b357e83109154f66003c37b150ac1b3c364ea41
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-07-31 22:01:28 +0530

    getMessage() in Exception

---
+++ OOPs/src/ExceptionHandling/TryCatch/Demo.java
@@ -0,0 +1,16 @@
+package ExceptionHandling.TryCatch;
+
+public class Demo {
+    static void main(String[] args) {
+        try{
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = a / b ;
+            System.out.println("Divide is "+c);
+        }catch(Exception hex){
+            System.out.println("Some error occurred! : "+hex.getMessage());
+            // method of Throwable class inherited by Exception class
+            //this method of String datatype and gives the reason behind the exception
+        }
+    }
+}
