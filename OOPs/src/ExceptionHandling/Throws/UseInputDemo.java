888a5ad51977f782cdf878e7e48ea72a68206b9e
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-07 19:56:00 +0530

    throws keyword

--- OOPs/src/ExceptionHandling/Throws/UseInputDemo.java
+++ OOPs/src/ExceptionHandling/Throws/UseInputDemo.java
@@ -1,4 +1,11 @@
 package ExceptionHandling.Throws;
 
+import java.io.IOException;
+
 public class UseInputDemo {
+    static void main(String[] args) throws IOException {
+        InputDemo hex = new InputDemo();
+        hex.acceptInt();
+        hex.acceptChar();//handle the Exception or use throws Keyword with Exception class
+    }
 }
