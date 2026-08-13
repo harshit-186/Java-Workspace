a0fef74c8ae3a27dd4272e6a87e02f3f9b1d87ee
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-13 20:28:29 +0530

    Override equals()

---
+++ StringHandling/src/String_Methods/Box.java
@@ -0,0 +1,18 @@
+package String_Methods;
+
+public class Box {
+    private int l , b , h ;
+
+    public Box(int l, int b, int h) {
+        this.l = l;
+        this.b = b;
+        this.h = h;
+    }
+    public String toString(){
+        return "Box";
+    }
+    public boolean equals (Object o) {
+        Box p = (Box) o;
+        if(this.l= p.l)
+    }
+}
