625360f4c5315b8b0e3bc7ef9bfa9f3d6df644db
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-15 21:05:51 +0530

    compareTo() in String

---
+++ StringHandling/src/String_Methods/CompareTo.java
@@ -0,0 +1,20 @@
+package String_Methods;
+
+public class CompareTo {
+    static void main(String[] args) {
+        String s1 = new String("AJAY");
+        String s2 = new String("AJIT");
+        String s3 = new String("ajit");
+        String s4 = new String("AJAY");
+
+
+        //case-sensitive (+ve defines larger string)
+        System.out.println(s2.compareTo(s1));//gives unicode 8
+        System.out.println(s1.compareTo(s2));//-8
+        System.out.println(s2.compareTo(s3));//-32
+        System.out.println(s1.compareTo(s4));//0 on equals
+
+
+
+    }
+}
