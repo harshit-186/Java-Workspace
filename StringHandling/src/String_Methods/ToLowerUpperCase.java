290b85bcab51294f770a41401416aa20f43fcaa3
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-16 11:27:14 +0530

    toLower & toUpperCase () in String

---
+++ StringHandling/src/String_Methods/ToLowerUpperCase.java
@@ -0,0 +1,12 @@
+package String_Methods;
+
+public class ToLowerUpperCase {
+    static void main(String[] args) {
+
+        String str = "BhOpAl";
+
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+        System.out.println(str);
+    }
+}
