c3935dca25bdbf02260e471a0ae380f695c7e6ad
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-16 11:13:46 +0530

    substring in string

---
+++ StringHandling/src/String_Methods/SubString.java
@@ -0,0 +1,9 @@
+package String_Methods;
+
+public class SubString {
+    static void main(String[] args) {
+        String str = "INDUSTURY";
+        System.out.println(str.substring(2,6));//DUST
+        System.out.println(str.substring(2));//DUSTURY
+    }
+}
