47bb29782214151b02024f48d9c3cb96cacc1c1d
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-19 18:23:58 +0530

    contains() in String

---
+++ StringHandling/src/String_Methods/Contains.java
@@ -0,0 +1,12 @@
+package String_Methods;
+
+public class Contains {
+    static void main(String[] args) {
+
+        String str = "WElCOME BUDDY" ;
+
+        System.out.println(str.contains("COME"));//true
+        System.out.println(str.contains("come"));//false
+
+    }
+}
