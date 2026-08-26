5b95a29af9935eb469d20de7896a4031316b57c6
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-26 20:36:49 +0530

    currentThread() in MultiThreading

---
+++ MULTI-THREADING/src/Thread_Methods/CurrentThread.java
@@ -0,0 +1,9 @@
+package Thread_Methods;
+
+public class CurrentThread {
+    static void main(String[] args) {
+        Thread th = Thread.currentThread();//static method of Thread
+        System.out.println("Thread Details : "+th);// id, name, priority, group
+        System.out.println("Hello User");
+    }
+}
