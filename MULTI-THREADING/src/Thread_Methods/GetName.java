1f833db2cfead232cc2e2d3cc357b4882d48b7d9
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-08-29 22:33:36 +0530

    getName() in Thread

--- MULTI-THREADING/src/Thread_Methods/GetName.java
+++ MULTI-THREADING/src/Thread_Methods/GetName.java
@@ -1,11 +1,11 @@
 package Thread_Methods;
 
-public class SetName {
+public class GetName {
     static void main(String[] args) {
         Thread th = Thread.currentThread();
         System.out.println("Thread details : "+th);
         th.setName("Hex");
         System.out.println("After name changed : "+th);
-
+        System.out.println(th.getName());
     }
 }
