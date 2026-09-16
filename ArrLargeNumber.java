f818283f385d3a6cf80a09cc39965f61a7902f17
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-09-16 15:51:34 +0530

    Array large number

--- ArrLargeNumber.java
+++ ArrLargeNumber.java
@@ -1,7 +1,17 @@
 package Array;
 
+import java.util.Scanner;
+
 public class ArrLargeNumber {
     static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int large = 0;
+        int [] arr = new int [5];
+        for (int i =0 ; i < arr.length ; i++){
+            System.out.println("Enter number : ");
+            arr[i] = sc.nextInt();
+            large = Math.max(large , arr[i]);
+        }
+        System.out.println("Largest number is : "+large);
     }
 }
