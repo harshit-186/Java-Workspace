00c498735fbd6356cf6af3a8073b8b108af2fd3c
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-09-21 10:27:57 +0530

    Collection

---
+++ COLLECTION/Arrays/ToString.java
@@ -0,0 +1,10 @@
+package COLLECTION.Arrays;
+
+import java.util.Arrays;
+
+public class ToString {
+    static void main(String[] args) {
+        int [] arr = {10,20,30,40,50};
+        System.out.println(Arrays.toString(arr));
+    }
+}
