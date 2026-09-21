21f4fd5743943259ba6de2f81b40c8be1157cc73
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-09-21 09:40:29 +0530

    Collection

---
+++ src/Arrays/ToString.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+import java.util.Arrays;
+//Arrays class is utility class (all methods are static
+
+public class ToString {
+    public static void main (String[] args){
+        int [] arr = {10,20,30,40,50};
+        System.out.println(Arrays.toString(arr));//toString is overloaded method
+    }
+}
