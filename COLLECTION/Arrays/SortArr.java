2e9683544dcafb4904a7cc4e80b113e3651d9c93
Harshit Yadav <yaduvanshi8186@gmail.com>
2026-09-22 22:46:30 +0530

    Arrays.sort(arr)

---
+++ COLLECTION/Arrays/SortArr.java
@@ -0,0 +1,11 @@
+package COLLECTION.Arrays;
+
+import java.util.Arrays;
+
+public class SortArr {
+    static void main(String[] args) {
+        int [] arr = {10,50,90,30,40,70};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
